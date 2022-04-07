406499834a72b7547826f83415c20fbf14dea2e9
白福泉 <v-baifuquan@xiaomi.com>
2022-04-07 20:03:55 +0800

    feat:udpate

--- src/src/test/java/Test1.java
+++ src/src/test/java/Test1.java
@@ -1,12 +1,12 @@
 /**
  * @author : baifuquan
- * @date : 2022/4/7 19:50
- * @className : test
- * @description : test
+ * @date : 2022/4/7 20:01
+ * @className : Test1
+ * @description : Test1
  **/
-public class test {
-
+public class Test1 {
     public static void main(String args[]) {
+        System.out.println(11);
         System.out.println(11);
         System.out.println(11);
     }
