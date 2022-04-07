394759c20af2c34c49c0ac3cdbfe6139a4258ef8
白福泉 <v-baifuquan@xiaomi.com>
2022-04-07 19:54:24 +0800

    feat:update

---
+++ src/src/test/java/test.java
@@ -0,0 +1,12 @@
+/**
+ * @author : baifuquan
+ * @date : 2022/4/7 19:50
+ * @className : test
+ * @description : test
+ **/
+public class test {
+
+    public static void main(String args[]) {
+        System.out.println(11);
+    }
+}
